78deb38d8c8dd792ab2c212fce4e86509d38381d
Sri Kannan <sri.kannan@creditkarma.com>
2017-07-08 23:29:07 -0700

    added CORS

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ const levenshtein = require('js-levenshtein');
 // Bootstrap application settings
 require('./config/express')(app);
 
+
+// Add headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 var personalityInsights = watson.personality_insights({
   username: '9c91bce0-4425-4ca5-9e0f-1dba6e00fbdd',
   password: 'I6VTma8dX1hs',
